1f25a9551073da25cdd3e2f64ef492bf08beae37
trungcuong123444 <Cuong_2051220106@dau.edu.vn>
2024-12-30 19:37:47 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
 import ParentPage from "./pages/profile/Profile";
-
+import firebase from "../src/context/Firebase/firebase.config";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    console.log(firebase);
+    
+  })
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
